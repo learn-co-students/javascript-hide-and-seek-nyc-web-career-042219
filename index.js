8f726cc9a33f896ce6a53b41159f61dcbef9c363
Jennifer Oh <ohjennifer.90@gmail.com>
2019-06-03 19:38:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector){
+    return document.querySelector(selector);
+}
 
+function nestedTarget(){
+    return document.querySelector('#nested .target');
+}
+
+function deepestChild(){
+    const node = document.getElementById('grand-node');
+    const moreNodes = node.querySelectorAll('div');
+    return moreNodes[3];
+};
+
+function increaseRankBy(n){
+    const node = document.querySelectorAll('.ranked-list li');
+    for (let i = 0; i < node.length; i++){
+        node[i].innerHTML = parseInt(node[i].innerHTML) + n
+    }  
+}
