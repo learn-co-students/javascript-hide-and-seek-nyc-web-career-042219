dab6806fb630ef5a223348fbfa6489f7ed9588a5
Augustus Kirby <kirbyaugustus@gmail.com>
2019-06-04 19:32:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+
+function nestedTarget(){
+  return document.querySelector("#nested .target")
+}
+
+function increaseRankBy(n){
+ document.querySelectorAll(".ranked-list > li")
+  forEach(function(el){
+  var cur = parseInt(el.innerText); 
+   cur += n;
+   el.innerText = cur
+  })
+}
+
+function deepestChild(){
+ return document.querySelector("#grand-node").lastElementChild
+}
