5254fc17629bff11147f298c60c29671774752fa
Qunli <qunli02@gmail.com>
2019-06-07 12:19:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+};
 
+function nestedTarget(){
+  return document.querySelector('#nested  .target')
+};
+
+function deepestChild() {
+  return document.querySelector('#grand-node  div div div div')
+};
+
+function increaseRankBy(n){
+   let all = document.querySelectorAll('.ranked-list li')
+   for (let i = 0; i < all.length; i++){
+     all[i].innerHTML = n + parseInt(all[i].innerHTML)
+   }
+};
